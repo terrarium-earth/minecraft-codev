4940a29118090c085e7f5d89f138c23fade78cf3
SettingDust <settingdust@gmail.com>
2025-03-05 13:45:59 +0800

    chore: bump the mercury to 0.4.2

--- minecraft-codev-remapper/build.gradle.kts
+++ minecraft-codev-remapper/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     api(group = "net.fabricmc", name = "mapping-io", version = "0.7.1")
     api(group = "net.fabricmc", name = "tiny-remapper", version = "0.11.0")
 
-    implementation(group = "org.cadixdev", name = "mercury", version = "0.1.0")
+    implementation(group = "net.fabricmc", name = "mercury", version = "0.4.2")
 
     // TODO Remap should be downloaded in a configuration and used with the exec operations service and potentially the workers API
     // implementation(group = "com.github.replaymod", name = "remap", "5134612")
