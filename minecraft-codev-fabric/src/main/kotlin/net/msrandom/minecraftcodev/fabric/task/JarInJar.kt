10f68e87380876544bba9b785d3e99f1cbbffe81
sery <sery@sappho.company>
2025-03-18 19:39:43 +0100

    extra task

--- minecraft-codev-fabric/src/main/kotlin/net/msrandom/minecraftcodev/fabric/task/JarInJar.kt
+++ minecraft-codev-fabric/src/main/kotlin/net/msrandom/minecraftcodev/fabric/task/JarInJar.kt
@@ -1,85 +1,27 @@
 package net.msrandom.minecraftcodev.fabric.task
 
-import kotlinx.serialization.json.*
-import net.msrandom.minecraftcodev.core.utils.getAsPath
-import net.msrandom.minecraftcodev.core.utils.isComponentFromDependency
-import net.msrandom.minecraftcodev.core.utils.zipFileSystem
-import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.component.ModuleComponentIdentifier
-import org.gradle.api.artifacts.result.ResolvedComponentResult
-import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.Internal
 import org.gradle.jvm.tasks.Jar
 import org.gradle.language.base.plugins.LifecycleBasePlugin
-import kotlin.io.path.*
-
-private val JSON: Json = Json { prettyPrint = true }
 
 abstract class JarInJar : Jar() {
-    abstract val includeConfiguration: Property<Configuration>
-        @Internal get
+    abstract val includedFiles: ConfigurableFileCollection
+        @InputFiles get
 
     abstract val input: RegularFileProperty
         @InputFile get
 
-    abstract val workingDirectory: DirectoryProperty
-        @Internal get
 
     init {
         group = LifecycleBasePlugin.BUILD_GROUP
 
         from(project.zipTree(input))
 
-        doFirst { generateFabricModJsons() }
-
-        from(workingDirectory) {
+        from(includedFiles) {
             it.into("META-INF/jars")
-        }
-    }
-
-    private fun generateFabricModJsons() {
-        val includes = includeConfiguration.get()
-        val dependencies = includes.incoming.resolutionResult.allComponents.map(ResolvedComponentResult::getId).associateWith { componentId ->
-            val dependency = includes.allDependencies.firstOrNull { dependency ->
-                isComponentFromDependency(componentId, dependency)
-            }
-
-            dependency
-        }
-
-        val dir = workingDirectory.getAsPath().createDirectories()
-
-        includes.incoming.artifacts.forEach {
-            val dependency = dependencies[it.id.componentIdentifier] ?: return@forEach
-            val module = it.id.componentIdentifier as? ModuleComponentIdentifier
-            val copy = it.file.toPath().copyTo(dir.resolve(it.file.name), true)
-
-            zipFileSystem(copy).use { fs ->
-                val fabricmodjson = fs.getPath("fabric.mod.json")
-                if (fabricmodjson.exists()) return@forEach
-
-                val group = (module?.group ?: dependency.group)?.replace('.', '_')
-                val name = module?.module ?: dependency.name
-                val version = module?.version ?: dependency.version ?: "0.0.0"
-
-                val json = buildJsonObject {
-                    put("schemaVersion", 1)
-                    put("id", group?.let { "${group}_$name" } ?: name)
-                    put("version", version)
-                    put("name", name)
-                    put("custom", buildJsonObject {
-                        put("fabric-loom:generated", true)
-                    })
-                }
-
-                fabricmodjson.createFile().outputStream().use {
-                    JSON.encodeToStream(json, it)
-                }
-            }
         }
     }
 }
