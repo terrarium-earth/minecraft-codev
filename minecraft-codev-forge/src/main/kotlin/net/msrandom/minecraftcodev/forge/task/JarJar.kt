dab0cefc3dd257a7a75a82d7a4785e894dc0a266
Ashley Wright <ashley@msrandom.net>
2025-05-25 22:31:48 +0300

    Make include tasks configuration cache safe

--- minecraft-codev-forge/src/main/kotlin/net/msrandom/minecraftcodev/forge/task/JarJar.kt
+++ minecraft-codev-forge/src/main/kotlin/net/msrandom/minecraftcodev/forge/task/JarJar.kt
@@ -1,43 +1,35 @@
 package net.msrandom.minecraftcodev.forge.task
 
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.addJsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.encodeToStream
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonArray
+import kotlinx.serialization.json.putJsonObject
+import net.msrandom.minecraftcodev.core.MinecraftCodevPlugin.Companion.json
 import net.msrandom.minecraftcodev.core.utils.getAsPath
-import net.msrandom.minecraftcodev.core.utils.isComponentFromDependency
-import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.ExternalDependency
-import org.gradle.api.artifacts.component.ModuleComponentIdentifier
-import org.gradle.api.artifacts.result.ResolvedComponentResult
+import net.msrandom.minecraftcodev.includes.IncludedJarInfo
+import net.msrandom.minecraftcodev.includes.IncludesJar
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.*
-import org.gradle.jvm.tasks.Jar
 import org.gradle.language.base.plugins.LifecycleBasePlugin
 import kotlin.io.path.deleteIfExists
+import kotlin.io.path.outputStream
 
-abstract class JarJar : Jar() {
-    abstract val includesConfiguration: Property<Configuration>
-        @Internal
-        get
-
-    abstract val input: RegularFileProperty
-        @InputFile
-        get
-
-    abstract val metadata: RegularFileProperty
+abstract class JarJar : IncludesJar() {
+    abstract val metadataOutput: RegularFileProperty
         @Internal get
 
     init {
         group = LifecycleBasePlugin.BUILD_GROUP
 
-        metadata.set(temporaryDir.resolve("metadata.json"))
+        metadataOutput.set(temporaryDir.resolve("metadata.json"))
 
-        from(metadata) {
+        from(metadataOutput) {
             it.into("META-INF/jarjar")
         }
 
-        from(includesConfiguration) {
+        from(includeArtifacts) {
             it.into("META-INF/jars")
         }
 
@@ -47,63 +39,38 @@ abstract class JarJar : Jar() {
     }
 
     private fun generateMetadata() {
-        val includes = includesConfiguration.get()
+        val includes = includeArtifacts.get()
 
-        if (includes.incoming.artifacts.artifacts.isEmpty()) {
-            metadata.getAsPath().deleteIfExists()
+        if (includes.isEmpty()) {
+            metadataOutput.getAsPath().deleteIfExists()
 
             return
         }
 
-        val dependencies = includes.incoming.resolutionResult.allComponents.map(ResolvedComponentResult::getId).associateWith { componentId ->
-            val dependency = includes.allDependencies.firstOrNull { dependency ->
-                isComponentFromDependency(componentId, dependency)
-            }
+        val info = IncludedJarInfo.fromResolutionResults(includesRootComponent.get(), includeArtifacts.get(), logger)
 
-            dependency
+        val metadata = buildJsonObject {
+            putJsonArray("jars") {
+                for (jar in info) {
+                    addJsonObject {
+                        putJsonObject("identifier") {
+                            put("group", jar.group)
+                            put("artifact", jar.moduleName)
+                        }
+
+                        putJsonObject("version") {
+                            put("range", jar.versionRange)
+                            put("artifactVersion", jar.artifactVersion)
+                        }
+
+                        put("path", "META-INF/jars/${jar.file.name}")
+                    }
+                }
+            }
         }
 
-        JsonObject(
-            mapOf(
-                "jars" to JsonArray(includes.incoming.artifacts.map {
-                    val dependency = dependencies[it.id.componentIdentifier]
-                    val module = it.id.componentIdentifier as? ModuleComponentIdentifier
-
-                    val constraintVersion = if (dependency is ExternalDependency) {
-                        val strict = dependency.versionConstraint.strictVersion.takeUnless(String::isEmpty)
-                        val required = dependency.versionConstraint.requiredVersion.takeUnless(String::isEmpty)
-                        val preferred = dependency.versionConstraint.preferredVersion.takeUnless(String::isEmpty)
-
-                        strict ?: required ?: preferred
-                    } else {
-                        null
-                    }
-
-                    val versionRange = constraintVersion
-                        ?: dependency?.version
-                        ?: module?.version
-                        ?: ""
-
-                    JsonObject(
-                        mapOf(
-                            "identifier" to JsonObject(
-                                mapOf(
-                                    "group" to JsonPrimitive(module?.group ?: dependency?.group.orEmpty()),
-                                    "artifact" to JsonPrimitive(module?.module ?: dependency?.name ?: it.file.nameWithoutExtension),
-                                )
-                            ),
-                            "version" to JsonObject(
-                                mapOf(
-                                    "range" to JsonPrimitive(versionRange),
-                                    "artifactVersion" to JsonPrimitive(module?.version ?: dependency?.version)
-                                )
-                            ),
-
-                            "path" to JsonPrimitive("META-INF/jars/${it.file.name}"),
-                        )
-                    )
-                })
-            )
-        )
+        metadataOutput.getAsPath().outputStream().use {
+            json.encodeToStream(metadata, it)
+        }
     }
 }
