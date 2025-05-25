a420f7a8b4075aad0985c9110feed9c8b053ef6b
Ashley Wright <ashley@msrandom.net>
2025-05-25 22:29:58 +0300

    Move intersection logic

--- minecraft-codev-intersections/src/main/kotlin/net/msrandom/minecraftcodev/intersection/MinecraftCodevIntersectionPlugin.kt
+++
@@ -1,9 +0,0 @@
-package net.msrandom.minecraftcodev.intersection
-
-import net.msrandom.minecraftcodev.core.utils.applyPlugin
-import org.gradle.api.Plugin
-import org.gradle.api.plugins.PluginAware
-
-class MinecraftCodevIntersectionPlugin<T : PluginAware> : Plugin<T> {
-    override fun apply(target: T) = applyPlugin(target)
-}
