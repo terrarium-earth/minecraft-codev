d03bfbc790b2d371c065a1fb82197db85303e86e
Ashley Wright <ashley@msrandom.net>
2025-07-26 09:54:28 +0300

    Add separator option for WriteClasspathFile

--- minecraft-codev-runs/src/main/kotlin/net/msrandom/minecraftcodev/runs/task/WriteClasspathFile.kt
+++ minecraft-codev-runs/src/main/kotlin/net/msrandom/minecraftcodev/runs/task/WriteClasspathFile.kt
@@ -4,13 +4,17 @@ import net.msrandom.minecraftcodev.core.utils.getAsPath
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import java.io.File
+import kotlin.io.path.bufferedWriter
 import kotlin.io.path.writeLines
 
 @CacheableTask
@@ -23,12 +27,35 @@ abstract class WriteClasspathFile : DefaultTask() {
         @PathSensitive(PathSensitivity.ABSOLUTE)
         get
 
+    abstract val separator: Property<String>
+        @Optional
+        @Input
+        get
+
     init {
         output.set(temporaryDir.resolve("classpath.txt"))
+        separator.convention("\n")
     }
 
     @TaskAction
     fun generate() {
-        output.getAsPath().writeLines(classpath.map(File::getAbsolutePath))
+        output.getAsPath().bufferedWriter().use { writer ->
+
+            val iterator = classpath.iterator()
+
+            if (!iterator.hasNext()) {
+                return
+            }
+
+            do {
+                writer.write(iterator.next().absolutePath)
+
+                if (iterator.hasNext()) {
+                    writer.write(separator.get())
+                } else {
+                    break
+                }
+            } while (true)
+        }
     }
 }
